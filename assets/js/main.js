02b1d412d359ce79792ecd9b0e9039a05eb02e61
Kimani Ndegwa <kimani.ndegwa@andela.com>
2017-01-24 15:42:32 +0300

    Fixes modal placement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 var navElement = document.getElementsByClassName('ka-trigger')[0];
 var body = document.getElementsByTagName('body')[0];
+var openDiv = document.getElementsByClassName('openDiv')[0];
+var checkOutBtn = document.getElementById('checkOutBtn');
 
 navElement.addEventListener('click', toggleNavigation)
 
@@ -7,3 +9,15 @@ function toggleNavigation(event){
   event.preventDefault();
   body.classList.toggle('nav-open')
 };
+
+var subBtn = document.getElementById('submit-btn');
+
+subBtn.addEventListener('click', closeModal)
+
+function closeModal(event){
+  event.preventDefault()
+  openDiv.classList.toggle('modalDialog')
+  checkOutBtn.classList.toggle('modalDialog')
+
+
+}
